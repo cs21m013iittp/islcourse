8cbc8da9ed0cd26de2f12f600a71e7e5d9fc694c
cs21m013iittp <116102710+cs21m013iittp@users.noreply.github.com>
2022-10-25 19:11:19 +0530

    Update hobconf.py

--- hobconf.py
+++ hobconf.py
@@ -5,56 +5,10 @@ def kali():
   print ('kali')
   
 # Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
-class cs21m013(nn.Module):
+class YourRollNumberNN(nn.Module):
   pass
   # ... your code ...
   # ... write init and forward functions appropriately ...
-  def __init__(self, ch, H, W, num_classes):
-    super(cs21m013, self).__init__()
-    self.conv_layers = nn.Sequential(
-        nn.Conv2d(ch, 8, 3, padding='same'),
-        nn.Conv2d(8, 16, 3, padding='same'),
-        )
-    
-        in_features = 16 * H * W
-        self.fc = nn.Sequential(
-            nn.Linear(in_features, 2048),
-            nn.Linear(2048, 1024),
-            nn.Linear(1024, 512),
-            nn.Linear(512, 256),
-            nn.Linear(256, num_classes)
-            )
-
-  def forward(self, x):
-    feat_maps = self.conv_layers(x)
-    feats = nn.Flatten(start_dim=1)(feat_maps)
-    logits = self.fc(feats)
-   
-    return logits
-
-
-model = cs21m013(ch, H, W, num_classes)
-model.to(device)
-optimizer = optim.Adam(model.parameters())
-
-total_loss = 0.0
-correct = 0
-for X_b, y_b in tqdm(train_data_loader):
-  X_b, y_b = X_b.to(device), y_b.to(device)
-  logits = model(X_b)
-  loss = nn.CrossEntropyLoss()(logits, y_b)
-  optimizer.zero_grad()
-  loss.backward()
-  optimizer.step()
-  
-  total_loss += loss.item()
-  probs: torch.Tensor = nn.Softmax(dim=1)(logits)
-  preds = probs.argmax(dim=1)
-  correct += (preds == y_b).sum().item()
-    
-
-  print(f"Accuracy of the model: {correct/len(train_data_loader.dataset)}")
-  return model
     
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model(train_data_loader=None, n_epochs=10):
@@ -66,7 +20,6 @@ def get_model(train_data_loader=None, n_epochs=10):
   # ... and so on ...
   # Use softmax and cross entropy loss functions
   # set model variable to proper object, make use of train_data
-  
   
   print ('Returning model... (rollnumber: xx)')
   
@@ -108,30 +61,7 @@ def test_model(model1=None, test_data_loader=None):
   # ... your code ...
   # ... and so on ...
   # calculate accuracy, precision, recall and f1score
- 
-  device = 'cuda' if torch.cuda.is_available() else 'cpu'
   
-  total_loss = 0.0
-  correct = 0
-  for X_b, y_b in test_data_loader:
-    X_b, y_b = X_b.to(device), y_b.to(device)
-    logits = model1(X_b)
-    loss = nn.CrossEntropyLoss()(logits, y_b)
-    total_loss += loss.item()
-
-    probs = nn.Softmax(dim=1)(logits)
-    preds = probs.argmax(dim=1)
-
-    correct = (preds == y_b).sum().item()
-
-    preds = preds.cpu().detach().numpy()
-    y_b = y_b.cpu().detach().numpy()
-
-
-    accuracy_val = correct / len(test_data_loader.dataset)
-    precision_val = precision_score(y_b, preds, average='macro')
-    recall_val = recall_score(y_b, preds, average='macro')
-    f1score_val = f1_score(y_b, preds, average='macro')
-    print ('Returning metrics... (cs21m013: xx)')
+  print ('Returning metrics... (rollnumber: xx)')
   
   return accuracy_val, precision_val, recall_val, f1score_val

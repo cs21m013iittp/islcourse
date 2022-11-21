e01435a443eb997ea34de34905a5b020052f712a
cs21m013iittp <116102710+cs21m013iittp@users.noreply.github.com>
2022-11-21 09:39:44 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -23,7 +23,7 @@ def get_data_circles(n_points=100):
   pass
   # write your code here
   # Refer to sklearn data sets
-  X, y = None
+  X, y = make_circles(n_samples=n_points)
   # write your code ...
   return X,y
 

a2823c08689c32fa6aabe370aea90dd3808c34f9
cs21m013iittp <116102710+cs21m013iittp@users.noreply.github.com>
2022-11-21 09:31:09 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -15,7 +15,7 @@ def get_data_blobs(n_points=100):
   pass
   # write your code here
   # Refer to sklearn data sets
-  X, y = None
+  X, y = make_blobs(n_samples=n_points)
   # write your code ...
   return X,y
 

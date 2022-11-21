36475c102c547a87381b7dddb9f09c4bb75a4e2a
cs21m013iittp <116102710+cs21m013iittp@users.noreply.github.com>
2022-11-21 12:40:47 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,116 +1,4 @@
 # kali
-import torch
-from torch import nn
-import torch.optim as optim
-from sklearn.datasets import make_blobs
-from sklearn.datasets import make_circles
-from sklearn.datasets import load_digits
-from sklearn.cluster import KMeans
-from sklearn.metrics import homogeneity_score,completeness_score,v_measure_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report,recall_score,roc_auc_score,precision_score,f1_score
-from sklearn.model_selection import GridSearchCV
-from sklearn import preprocessing
-from sklearn import model_selection
-
-
-# You can import whatever standard packages are required
-
-# full sklearn, full pytorch, pandas, matplotlib, numpy are all available
-# Ideally you do not need to pip install any other packages!
-# Avoid pip install requirement on the evaluation program side, if you use above packages and sub-packages of them, then that is fine!
-
-###### PART 1 ######
-
-import torch
-import numpy as np
-import sklearn
-from sklearn.datasets import make_blobs,make_circles,load_digits
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.metrics import homogeneity_score,completeness_score,v_measure_score
-
-
-
-def get_data_blobs(n_points=100):
-  # write your code here
-  # Refer to sklearn data sets
-  X, y = make_blobs(n_samples=n_points)
-  # write your code ...
-  return X,y
-
-def get_data_circles(n_points=100):
-  pass
-  # write your code here
-  # Refer to sklearn data sets
-  X, y = make_circles(n_samples=n_points)
-  # write your code ...
-  return X,y
-
-def get_data_mnist():
-  # write your code here
-  # Refer to sklearn data sets
-  data = load_digits()
-  X = data.images
-  y = data.target
-  # write your code ...
-  return X,y
-
-def build_kmeans(X=None,k=10):
-  
-  # k is a variable, calling function can give a different number
-  # Refer to sklearn KMeans method
-  km = KMeans(n_clusters=k) # this is the KMeans object
-  # write your code ...
-  km.fit(X)
-  return km
-
-
-def assign_kmeans(km=None,X=None):
-  # For each of the points in X, assign one of the means
-  # refer to predict() function of the KMeans in sklearn
-  # write your code ...
-  ypred = km.predict(X)
-  return ypred
-
-def compare_clusterings(ypred_1=None,ypred_2=None):
-  # refer to sklearn documentation for homogeneity, completeness and vscore
-  h,c,v = 0,0,0 # you need to write your code to find proper values
-
-  h = homogeneity_score(ypred_1,ypred_2)
-  c = completeness_score(ypred_1,ypred_2)
-  v = v_measure_score(ypred_1,ypred_2)
-  return h,c,v
-
-###### PART 2 ######
-
-
-import torch
-from torch import nn
-import torch.optim as optim
-from sklearn.datasets import make_blobs
-from sklearn.datasets import make_circles
-from sklearn.datasets import load_digits
-from sklearn.cluster import KMeans
-from sklearn import metrics
-from sklearn.metrics import homogeneity_score,completeness_score,v_measure_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report,recall_score,roc_auc_score,precision_score,f1_score
-from sklearn.model_selection import GridSearchCV
-
-
-
-
-
 # You can import whatever standard packages are required
 
 # full sklearn, full pytorch, pandas, matplotlib, numpy are all available
@@ -170,10 +58,6 @@ def compare_clusterings(ypred_1=None,ypred_2=None):
   c=completeness_score(ypred_1,ypred_2)
   v=v_measure_score(ypred_1,ypred_2)
   
-  print(h)
-  print(c)
-  print(v)
-  
   return h,c,v
 
 ###### PART 2 ######
@@ -199,11 +83,6 @@ def get_metrics(model1,X,y):
   
   y_test=y
   y_pred_test = model1.predict(X)
-  
-  
-  
-  
-  
   # View accuracy score
   acc=accuracy_score(y_test, y_pred_test)
   # print(acc)
@@ -222,8 +101,6 @@ def get_metrics(model1,X,y):
   
   return acc, prec, rec, f1, auc
 
-
-# part 2b ...
 
 
 def get_paramgrid_lr():

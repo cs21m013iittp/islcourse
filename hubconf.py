43b5d7cd41a1640893010d81c994396554215a82
cs21m013iittp <116102710+cs21m013iittp@users.noreply.github.com>
2022-11-21 11:51:01 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -23,114 +23,47 @@ from sklearn.model_selection import GridSearchCV
 
 ###### PART 1 ######
 
-def get_data_blobs(n_points=100):
-  pass
-  # write your code here
-  # Refer to sklearn data sets
-  X, y = make_blobs(n_samples=n_points)
-  # write your code ...
-  return X,y
-
-def get_data_circles(n_points=100):
-  pass
-  # write your code here
-  # Refer to sklearn data sets
-  X, y = make_circles(n_samples=n_points)
-  # write your code ...
-  return X,y
-
-def get_data_mnist():
-  pass
-  # write your code here
-  # Refer to sklearn data sets
-   #X,y = None
-  # write your code ...
-  digits= load_digits()
-  X = digits.data
-  y = digits.target
-  return X,y
-
-def build_kmeans(X=None,k=10):
-  pass
-  # k is a variable, calling function can give a different number
-  # Refer to sklearn KMeans method
-  km = KMeans(n_clusters=k).fit(X)# this is the KMeans object
-  # write your code ...
-  return km
-
-def assign_kmeans(km=None,X=None):
-  pass
-  # For each of the points in X, assign one of the means
-  # refer to predict() function of the KMeans in sklearn
-  # write your code ...
-  ypred = km.predict(X)
-  return ypred
-
-def compare_clusterings(ypred_1=None,ypred_2=None):
-  pass
-  # refer to sklearn documentation for homogeneity, completeness and vscore
-  h=homogeneity_score(ypred_1,ypred_2)
-  c=completeness_score(ypred_1,ypred_2)
-  v=v_measure_score(ypred_1,ypred_2)# you need to write your code to find proper values
-  return h,c,v
-
-###### PART 2 ######
 def build_lr_model(X=None, y=None):
-  lr_model = LogisticRegression()
   # write your code...
   # Build logistic regression, refer to sklearn
-  if X.ndim > 2:
-      n_samples = len(X)
-      X= X.reshape((n_samples, -1))
-  lr_model.fit(X,y)
+  scaler = preprocessing.StandardScaler().fit(X)
+  X_train = scaler.transform(X)
+  lr_model = None
+  lr_model = LogisticRegression(random_state=0).fit(X_train, y)
   return lr_model
 
 def build_rf_model(X=None, y=None):
-  rf_model = RandomForestClassifier()
   # write your code...
   # Build Random Forest classifier, refer to sklearn
-  if X.ndim > 2:
-      n_samples = len(X)
-      X= X.reshape((n_samples, -1))
-  rf_model.fit(X,y)
+  rf_model = None
+  rf_model = RandomForestClassifier(random_state=0, max_depth=5).fit(X, y)
   return rf_model
 
-
-def get_metrics(model=None,X=None,y=None):
+def get_metrics(model1=None,X=None,y=None):
   # Obtain accuracy, precision, recall, f1score, auc score - refer to sklearn metrics
-  
-  if X.ndim > 2:
-      n_samples = len(X)
-      X= X.reshape((n_samples, -1))
-  classes = set()
-  for i in y:
-      classes.add(i)
-  num_classes = len(classes)
-
-  ypred = model.predict(X)
   acc, prec, rec, f1, auc = 0,0,0,0,0
   # write your code here...
-  acc = accuracy_score(y,ypred)
-  if num_classes == 2:
-    prec = precision_score(y,ypred)
-    recall = recall_score(y,ypred)
-    f1 = f1_score(y,ypred)
-    auc = roc_auc_score(y,ypred)
+  ypred = model1.predict(X)
 
-  else:
-    prec = precision_score(y,ypred,average='macro')
-    recall = recall_score(y,ypred,average='macro')
-    f1 = f1_score(y,ypred,average='macro')
-    pred_prob = model.predict_proba(X)
-    roc_auc_score(y, pred_prob, multi_class='ovr')
-    auc = roc_auc_score(y,ypred,average='macro',multi_class='ovr')
-
+  acc = metrics.accuracy_score(y, ypred)
+  prec = metrics.precision_score(y, ypred, average='macro')
+  rec = metrics.recall_score(y, ypred, average='macro')
+  f1 = metrics.f1_score(y, ypred, average='macro')
+  fpr, tpr, thresholds = metrics.roc_curve(y, ypred, pos_label=2)
+  auc = metrics.auc(fpr, tpr)
   return acc, prec, rec, f1, auc
 
 def get_paramgrid_lr():
   # you need to return parameter grid dictionary for use in grid search cv
   # penalty: l1 or l2
-  lr_param_grid = {'penalty' : ['l1','l2']}
+  lr_param_grid = None
+  lr_param_grid = {
+    'penalty': ['l1', 'l2'],
+    'C': np.logspace(-4, 4, 20),
+    # 'C': [1.0, 2.0],
+    'solver': ['liblinear'],
+
+  }
   # refer to sklearn documentation on grid search and logistic regression
   # write your code here...
   return lr_param_grid
@@ -140,37 +73,41 @@ def get_paramgrid_rf():
   # n_estimators: 1, 10, 100
   # criterion: gini, entropy
   # maximum depth: 1, 10, None  
-  rf_param_grid = { 'n_estimators' : [1,10,100],'criterion' :["gini", "entropy"], 'max_depth' : [1,10,None]  }
+  rf_param_grid = None
+  rf_param_grid = {
+    'n_estimators': list(range(10,101,10)),
+    'max_features': list(range(6,32,5)),
+    'criterion': ['gini', 'entropy', 'log_loss'],
+    'max_depth': [2, 5, 10, None]
+  }
   # refer to sklearn documentation on grid search and random forest classifier
   # write your code here...
   return rf_param_grid
 
-def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
+def perform_gridsearch_cv_multimetric(model1=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc_ovo', 'roc_auc_ovr']):
   
   # you need to invoke sklearn grid search cv function
   # refer to sklearn documentation
   # the cv parameter can change, ie number of folds  
   
   # metrics = [] the evaluation program can change what metrics to choose
-
+  
   # create a grid search cv object
   # fit the object on X and y input above
   # write your code here...
-  
+  grid_search_cv = None
+  grid_search_cv = model_selection.GridSearchCV(model1, param_grid, cv=cv, scoring=metrics, refit=False).fit(X, y)
+  cv_results = grid_search_cv.cv_results_
   # metric of choice will be asked here, refer to the-scoring-parameter-defining-model-evaluation-rules of sklearn documentation
-  
+
   # refer to cv_results_ dictonary
   # return top 1 score for each of the metrics given, in the order given in metrics=... list
   
   top1_scores = []
+
+  for metric in metrics:
+    i = list(cv_results[f'rank_test_{metric}']).index(1)
+    top_score = list(cv_results[f'mean_test_{metric}'])[i]
+    top1_scores.append(top_score)
   
-  if X.ndim > 2:
-      n_samples = len(X)
-      X= X.reshape((n_samples, -1))
-      
-  for score in metrics:
-      grid_search_cv = GridSearchCV(model,param_grid,scoring = score,cv=cv)
-      grid_search_cv.fit(X,y)
-      top1_scores.append(grid_search_cv.best_estimator_.get_params())
-      
   return top1_scores
